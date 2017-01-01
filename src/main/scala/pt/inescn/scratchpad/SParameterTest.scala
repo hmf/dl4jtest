e4ff2af1a6e947eef1b5c5a1b9dce9db36dd724f
Hugo Ferreira <hmf@inesctec.pt>
2017-01-01 10:51:43 +0000

    Higher-kineded type example ready. TODO: place example in appropriate package TOOD: adapt existing Parameter code to use higher-kinded type

--- src/main/scala/pt/inescn/scratchpad/SParameterTest.scala
+++ src/main/scala/pt/inescn/scratchpad/SParameterTest.scala
@@ -106,18 +106,65 @@ case class SlearningRate( override val value: Double = 0.0) extends SParameter[ 
   def apply( v: Double ) = new SlearningRate( v ) 
   }
 
-class ChangeOption[P](v : Option[P]) {
-  
-  def this() {
-    this(None)
-  }
-  
-  def to[Q](nv:  Q ) : ChangeOption[Q] = new ChangeOption( Some(nv) )
+
+/**
+ * A basic container type that can hold any primitive or complex type
+ */
+case class AP[T](val v: T) {
+  type I = T
 }
 
-/*
-trait SParameterRange[ P <: SParameter[ _ ], U, B, E, C ] {
-*/
+import scala.language.higherKinds
+
+//class AS[P <: AP[_],T <: P#I](r : AP[T]) {
+//class AS[P <: AP[_],T <: P#I](r : AP[P#I]) {
+//class AS[P <: AP[_],T <: P#I](r : P) {
+//class AS[P <: AP[T],T <: P#I](r : P) {
+/**
+ * Use a higher-kinded type `P[T]` that allows us to extract the contained
+ * type `T` from the container `AP[T]`. In this way we can declare new types 
+ * based on the contained type `T`. We can alos use a contained type `U` that
+ * also enables run-time type conversion (checked at compilation time). 
+ * 
+ * NOTE: this version uses unsafe `var` for the generator - a call to `map`.
+ * may result in a null pointer exception.  Consider using the Safe Builder pattern. 
+ */
+class AS[P[T] <: AP[T],T,U](r : P[T], var generator : T => U) {
+  
+  type Gen = T => U
+  
+  def this(r : P[T]) {
+    this(r, null)
+  }
+  
+  def make[Q[S] <: AP[S],S](nv:  Q[S] ) : AS[Q,S,U] = new AS[Q,S,U](nv) 
+  def to[V](g: T => V) = { val t = new AS[P,T,V](r) ; t.generator = g; t }
+  def from : T           = r.v
+  def map : U            = generator( from )
+}
+
+/**
+ * Same as the `AS` type above but uses a safer Option types. This avoids
+ * the null pointer exception however  an exception will still be possible
+ * when we use the `Option.get` methods when `Nothing`is set.  
+ */
+class AR[P[T] <: AP[T],T,U](val r : Option[P[T]], val generator : Option[T => U]) {
+  
+  /* Possible
+  def this() {
+    this(None, None)
+  }*/
+  
+  def this(nr : Option[P[T]]) {
+    this(nr, None)
+  }
+  
+  def make[Q[S] <: AP[S],S](nv:  Q[S] ) : AR[Q,S,U] = new AR[Q,S,U]( Some(nv)) 
+  def to[V](g: T => V) = { new AR[P,T,V](r, Some(g))  }
+  def from : T           = r.get.v
+  def map : U            = generator.get( from )
+}
+
 
 object SParameterTest {
     
@@ -126,7 +173,7 @@ object SParameterTest {
       linspace( from.value, by ).map{ x => from.apply( x ) }.take(  len + 1)
     }
     
-// TODO: how to change P type when using default constructor 
+// TODO: Add phantom type G for the generator
 // TODO: How to make generators independent from type P 
 
   def main( args: Array[ String ] ) {
@@ -147,11 +194,49 @@ object SParameterTest {
     val s4 = s3 using( linSearch )
     val s5 = s4.toStream
     t5.foreach { x => println( x ) }
+
+    val ai = new AP(100)
+    val as = new AP("1000")
     
-    val u = new ChangeOption
-    val u1 = u to 100
     
-    val w2 = t.from(SlearningRate( 0.0 )).to(SlearningRate( 0.01 )).by(0.001 )
+    // Example of morphing higher-kined types in order to set and extract basic inner types
+    // We set the contained type with an Int, so the higher type holds an Int
+    val w = new AS(ai)                                              // AS[AP, Int, Nothing]
+    val a = w.from                                                    // Int
+    println(s"AP[Int] = $a")
+    // We change the contained type to a String, so the higher type holds a String
+    val w1 = w make as                                             // AS[AP, String, Nothing]
+    val a1 = w1.from                                                 // String
+    println(s"AP[String] = $a1")
+    // We set the mapping contained type to a String
+    val w2 = w1 to { x => x + "!"}                                // AS[AP, String, String]
+    val a2 = w2.map                                                  // Changed string
+    println(s"Mapped AP[String to String] = $a2")
+    // We set the mapping contained type to an Int
+    val w3 = w2 to { x => x.toInt * 10 }                     // AS[AP; String, Int]
+    val a3 = w3.map
+    println(s"Mapped AP[String to Int] = $a3")
+
+    // Same as above but using the `Option` instead of a `var`
+    // We set the contained type with an Int, so the higher type holds an Int
+    val v = new AR(Some(ai))                                    // AR[AP, Int, Nothing]
+    val b = v.from                                                    // Int
+    println(s"AR[Int] = $b")
+    // We change the contained type to a String, so the higher type holds a String
+    val v1 = v make as                                              // AR[AP, String, Nothing]
+    val b1 = v1.from                                                 // String
+    println(s"AR[String] = $b1")
+    // We set the mapping contained type to a String
+    val v2 = v1 to { x => x + "!"}                                // AR[AP, String, String]
+    val b2 = v2.map                                                  // Changed string
+    println(s"Mapped AR[String to String] = $a2")
+    // We set the mapping contained type to an Int
+    val v3 = v2 to { x => x.toInt * 10 }                     // AR[AP; String, Int]
+    val b3 = v3.map
+    println(s"Mapped AP[String to Int] = $a3")
+
+    
+    //val w2 = t.from(SlearningRate( 0.0 )).to(SlearningRate( 0.01 )).by(0.001 )
     //val w3 = w2 build()
     //val w4 = w3.toStream
     //val w4 = w2.toStream
