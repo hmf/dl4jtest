47e1a481c8cc777d8ab38d81d9c416cf45298fd5
Hugo Ferreira <hmf@inesctec.pt>
2017-01-06 10:30:56 +0000

    backup

--- src/main/scala/pt/inescn/scratchpad/SParameterTest.scala
+++ src/main/scala/pt/inescn/scratchpad/SParameterTest.scala
@@ -3,18 +3,10 @@ package pt.inescn.scratchpad
 
 import StreamBuilder._
 
+// Parameters are value classes
 
 /**
  *
- * TODO: use robust self builder. 
- * TODO: avoid the use of so many polymorphic parameters for each range parameter. Can we type states?
- * See Jim McBeath part 3
- * 
- * TODO: (parts 2,3 and 4)
- * @see http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-using-church.html
- * @see http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-2.html
- * @see http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-3.html
- * @see http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-4.html
  * @see http://stackoverflow.com/questions/19642053/when-to-use-val-or-def-in-scala-traits
  */
 trait Parameter[ T ] {
@@ -27,7 +19,7 @@ trait Parameter[ T ] {
 import scala.{ Option, Some, None }
 import scala.language.higherKinds
 
-trait ParameterRange[ P[T] <: Parameter[T], T, U, B, E, C, G ] {
+trait SParameterRange[ P[T] <: Parameter[ T], T, U, B, E, C, G ] {
   
   type Gen = (T, T, U) => Stream[ T ]
   
@@ -36,17 +28,18 @@ trait ParameterRange[ P[T] <: Parameter[T], T, U, B, E, C, G ] {
   val config:       Option[ U ]
   val generator : Option[  Gen ]
 
+  //def apply( begin: P[T]): SParameterRange[ P, T, U, BEGIN, TMISSING, TMISSING, TMISSING]
   def makeStreamable: SStreamableParameterRange[ P, T, U, B, E, C, G]
 
-  def from[Q[S] <: Parameter[S],S]( nbegin: Q[S] ): ParameterRange[ Q, S, U, BEGIN, E, C, G ]
-  def to( nend: P[T] ): ParameterRange[ P, T, U, B, END, C, G ]
-  def by( nconfig: U ): ParameterRange[ P, T, U, B, E, CONFIG, G ]
-  def using (gen: Gen) : ParameterRange[ P, T, U, B, E, C, GEN ]
+  def from[Q[S] <: Parameter[S],S]( nbegin: Q[S] ): SParameterRange[ Q, S, U, BEGIN, E, C, G ]
+  def to( nend: P[T] ): SParameterRange[ P, T, U, B, END, C, G ]
+  def by( nconfig: U ): SParameterRange[ P, T, U, B, E, CONFIG, G ]
+  def using (gen: Gen) : SParameterRange[ P, T, U, B, E, C, GEN ]
 }
 
-// Alternatively extends ParameterRange[ P, T, U, B, E, C, G ]
+// Alternatively extends SParameterRange[ P, T, U, B, E, C, G ]
 trait SStreamableParameterRange[ P[T] <: Parameter[T], T, U, B, E, C, G ]   {
-  this : ParameterRange[ P, T, U, B, E, C, G ] =>
+  this : SParameterRange[ P, T, U, B, E, C, G ] =>
   
   def toStream : Stream[P[T]#Self] = { 
     val f = generator.get
@@ -58,12 +51,12 @@ trait SStreamableParameterRange[ P[T] <: Parameter[T], T, U, B, E, C, G ]   {
   
 }
 
-trait SafeBuild[ P[T] <: Parameter[T], T] {
+trait SSafeBuild[ P[T] <: Parameter[T], T] {
   def build(): SStreamableParameterRange[ P, T, Double, BEGIN, END, CONFIG, GEN ]
 }
 
 // Can also use `abstract class`
-trait BEGIN
+ trait BEGIN
 trait END
 trait CONFIG
 trait GEN
@@ -71,10 +64,10 @@ trait MISSING
 
 import scala.language.implicitConversions
 
-object ParameterRange {
+object SParameterRange {
 
-  implicit def enableBuild[P[T] <: pt.inescn.scratchpad.Parameter[T],T,U]( range: ParameterRange[ P, T, Double, BEGIN, END, CONFIG, GEN ] ) = 
-    new SafeBuild[P, T] {
+  implicit def enableBuild[P[T] <: pt.inescn.scratchpad.Parameter[T],T,U]( range: SParameterRange[ P, T, Double, BEGIN, END, CONFIG, GEN ] ) = 
+    new SSafeBuild[P, T] {
       def build() : SStreamableParameterRange[ P, T, Double, BEGIN, END, CONFIG, GEN ] = 
       range.makeStreamable
       
@@ -82,12 +75,12 @@ object ParameterRange {
   
 }
 
-class BasicParameterRange[ P[T] <: Parameter[T], T, B, E, C, G ]( 
+private class SLinearParameterRange[ P[T] <: Parameter[T], T, B, E, C, G ]( 
                                                                           val begin:       Option[P[T]], 
                                                                           val end:          Option[P[T]], 
                                                                           val config:      Option[ Double ],
                                                                           val generator: Option[ (T,T,Double) => Stream[T]] )
-    extends ParameterRange[ P, T, Double, B, E, C, G ] {
+    extends SParameterRange[ P, T, Double, B, E, C, G ] {
   
   def this( ){
     this( None, None, None, None )
@@ -98,19 +91,19 @@ class BasicParameterRange[ P[T] <: Parameter[T], T, B, E, C, G ](
   }
   
   def makeStreamable: SStreamableParameterRange[ P, T, Double, B, E, C, G] = 
-    new BasicParameterRange[ P, T, B, E, C, G ]( begin, end, config, generator) with SStreamableParameterRange[ P, T, Double, B, E, C, G]
+    new SLinearParameterRange[ P, T, B, E, C, G ]( begin, end, config, generator) with SStreamableParameterRange[ P, T, Double, B, E, C, G]
   
-  def from[Q[S] <: Parameter[ S],S]( nbegin: Q[S] ): ParameterRange[ Q, S, Double, BEGIN, E, C, G ] 
-    = new BasicParameterRange[ Q, S, BEGIN, E, C, G ]( Some(nbegin), None, config, None)
+  def from[Q[S] <: Parameter[ S],S]( nbegin: Q[S] ): SParameterRange[ Q, S, Double, BEGIN, E, C, G ] 
+    = new SLinearParameterRange[ Q, S, BEGIN, E, C, G ]( Some(nbegin), None, config, None)
     
-  def to( nend: P[T] ): ParameterRange[ P, T, Double, B, END, C, G ] 
-    = new BasicParameterRange[ P,T,  B, END, C, G ]( begin, Some(nend), config, generator )
+  def to( nend: P[T] ): SParameterRange[ P, T, Double, B, END, C, G ] 
+    = new SLinearParameterRange[ P,T,  B, END, C, G ]( begin, Some(nend), config, generator )
 
-  def by( nconfig: Double ): ParameterRange[ P, T, Double, B, E, CONFIG, G ] 
-    = new BasicParameterRange[ P, T, B, E, CONFIG, G ]( begin, end, Some( nconfig ), generator )
+  def by( nconfig: Double ): SParameterRange[ P, T, Double, B, E, CONFIG, G ] 
+    = new SLinearParameterRange[ P, T, B, E, CONFIG, G ]( begin, end, Some( nconfig ), generator )
   
-  def using( gen : (T, T, Double) => Stream[ T ] ): ParameterRange[ P, T, Double, B, E, C, GEN ] =  {
-    new BasicParameterRange[ P, T, B, E, C, GEN]( begin, end, config, Some(gen) )
+  def using( gen : (T, T, Double) => Stream[ T ] ): SParameterRange[ P, T, Double, B, E, C, GEN ] =  {
+    new SLinearParameterRange[ P, T, B, E, C, GEN]( begin, end, config, Some(gen) )
   }
 }
 
@@ -124,14 +117,13 @@ case class ParamOne[T]( val value: T = 0.0) extends Parameter[ T ] {
   }
 
 /**
- * Another example parameter. 
  * Should not define these classes without the polymorphic parameter. If we do
  * then the types will only show the generic Parameter. See examples below. 
  */
-case class ParameterTwo( val value: Double = 0.0) extends Parameter[ Double ] { 
-  type Self = ParameterTwo
+case class XSlearningRate( val value: Double = 0.0) extends Parameter[ Double ] { 
+  type Self = XSlearningRate
 
-  def apply( v: Double ) : Self = new ParameterTwo( v ) 
+  def apply( v: Double ) : Self = new XSlearningRate( v ) 
   }
 
 object SParameterTest {
@@ -144,7 +136,7 @@ object SParameterTest {
   def main( args: Array[ String ] ) {
     
     val t0 = ParamOne()
-    val t  = new BasicParameterRange( t0 )
+    val t  = new SLinearParameterRange( t0 ) // TODO: Hide constructor and use companion object with apply
     //val x = t.toStream // TODO: compile should fail 
     val t1 = t from ParamOne( 0.0 )
     val t2 = t1 using( linSearch )
@@ -154,7 +146,7 @@ object SParameterTest {
     val t5 = t4.build().toStream 
     t5.foreach { x => println( x ) }
     
-    val s = new BasicParameterRange
+    val s = new SLinearParameterRange
     val s1 = s from t0
     val s2 = s1 to ParamOne( 1.0 )
     val s3 = s2 by 0.1
@@ -163,9 +155,9 @@ object SParameterTest {
     val s5 = t4.build().toStream 
     t5.foreach { x => println( x ) }
 
-    val u0 = ParameterTwo()
-    val u1  = new BasicParameterRange( u0 )
-    val u2 = u1.from(ParameterTwo(1.0)).to(ParameterTwo(3)).by(1).using(linSearch).build().toStream
+    val u0 = XSlearningRate()
+    val u1  = new SLinearParameterRange( u0 )
+    val u2 = u1.from(XSlearningRate(1.0)).to(XSlearningRate(3)).by(1).using(linSearch).build().toStream
     u2.foreach { x => println( x ) }
     // compilation fails
     //val u3 = u1.toStream  
