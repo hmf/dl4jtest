0f7e4e562578283248891e7aba88aab9106f25ec
Hugo Ferreira <hmf@inesctec.pt>
2017-01-06 14:18:57 +0000

    SatefullBuilderV1 and StatefullBuilderV2 done.

--- src/main/scala/pt/inescn/scratchpad/idioms/StatefullBuilderV2.scala
+++ src/main/scala/pt/inescn/scratchpad/idioms/StatefullBuilderV2.scala
@@ -2,33 +2,45 @@ package pt.inescn.scratchpad.idioms
 
 
 /**
- * This is an altered version of  the example from http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-4.html
- * For the original, see `StatefullBuilderV1`.
+ * This is an altered version of  the example from:
+ *    http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-4.html
+ * For the original, see `StatefullBuilderV1`. The main difference here is that we do not use structural types 
+ * and hence do not use code reflection. To do this we create and explicit `BaseState` class that has
+ * the state types. We then set each and every state type explicitly. This makes the code a little more
+ * verbose but is still very readable.  We also define a builder explicitly so that no anonymous class/object 
+ * is required. 
+ * 
+ * We removed superfluous code  regarding the Pyramid calculation. This code may now serve as a basis
+ * for other work.
+ * 
+ * The IDE is still able to help us with type error debugging. 
  */
 
-// http://jim-mcbeath.blogspot.pt/2009/09/type-safe-builder-in-scala-part-4.html
 import scala.language.implicitConversions
-//import scala.language.reflectiveCalls // TODO
 
-    //A small collection of class types to define a state machine that counts
-trait COUNTER              { type Count <: COUNTER }
-trait MANY extends COUNTER { type Count = MANY }
-trait TWO  extends COUNTER { type Count = MANY }
-trait ZERO_OR_ONE extends COUNTER
-trait ONE  extends ZERO_OR_ONE { type Count = TWO }
-trait ZERO extends ZERO_OR_ONE { type Count = ONE }
 
 object StateO {
+    //A small collection of class types to define a state machine that counts
+    trait COUNTER                                                { type Count <: COUNTER }
+    trait MANY               extends COUNTER          { type Count = MANY }
+    trait TWO                 extends COUNTER         { type Count = MANY }
+    trait ZERO_OR_ONE extends COUNTER
+    trait ONE                  extends ZERO_OR_ONE { type Count = TWO }
+    trait ZERO                 extends ZERO_OR_ONE { type Count = ONE }
 
-    class BaseState {  //self:BaseState =>
+    /* 
+     * Compiler-time state coded as a type
+     * Can be a class, abstract class or trait 
+     */
+    trait BaseState { 
       
       type COUNT_LENGTH <: COUNTER
       type COUNT_WIDTH <: COUNTER
       type COUNT_AREA <: COUNTER
-      type COUNT_BASE <: COUNTER          // length, width or area
+      type COUNT_BASE      <: COUNTER          // length, width or area
       type COUNT_HEIGHT <: COUNTER
-      type COUNT_EDGE <: COUNTER
-      type COUNT_VERT <: COUNTER          // height or edge
+      type COUNT_EDGE      <: COUNTER
+      type COUNT_VERT      <: COUNTER          // height or edge
     }
   
     //The class that manages the state of our specification
@@ -37,30 +49,6 @@ object StateO {
         //We maintain compiler-time state to count the two types of calls
        type ST <: BaseState
 
-       /*
-        //We maintain compiler-time state to count the two types of calls
-        type TT <: {
-            type COUNT_LENGTH <: COUNTER
-            type COUNT_WIDTH <: COUNTER
-            type COUNT_AREA <: COUNTER
-            type COUNT_BASE <: COUNTER          // length, width or area
-            type COUNT_HEIGHT <: COUNTER
-            type COUNT_EDGE <: COUNTER
-            type COUNT_VERT <: COUNTER          // height or edge
-        }
-       */
-       /*
-        class SpecsWith extends Base {
-        }*/
-
-        //def setLength = new SpecsWith {
-        /*def setLength = new Base {
-            type TT = self.TT {
-                type COUNT_LENGTH = self.TT#COUNT_LENGTH#Count
-                type COUNT_BASE = self.TT#COUNT_BASE#Count
-            }
-        }*/
-        
         def setSLength = new Base {
             type ST = BaseState {
                 type COUNT_LENGTH = self.ST#COUNT_LENGTH#Count
@@ -72,14 +60,6 @@ object StateO {
                 type COUNT_VERT      = self.ST#COUNT_VERT // height or edge
             }
         }
-
-        //def setWidth = new SpecsWith {
-       /*        def setWidth = new Base {
-            type TT = self.TT {
-                type COUNT_WIDTH = self.TT#COUNT_WIDTH#Count
-                type COUNT_BASE = self.TT#COUNT_BASE#Count
-            }
-        }*/
 
         def setSWidth = new Base {
             type ST = BaseState {
@@ -93,14 +73,6 @@ object StateO {
             }
         }
         
-        //def setArea = new SpecsWith {
-        /*def setArea = new Base {
-            type TT = self.TT {
-                type COUNT_AREA = self.TT#COUNT_AREA#Count
-                type COUNT_BASE = self.TT#COUNT_BASE#Count
-            }
-        }*/
-        
         def setSArea = new Base {
             type ST = BaseState  {
                 type COUNT_LENGTH  = self.ST#COUNT_LENGTH
@@ -113,14 +85,6 @@ object StateO {
             }
         }
 
-        //def setHeight = new SpecsWith {
-        /*def setHeight = new Base {
-            type TT = self.TT {
-                type COUNT_HEIGHT = self.TT#COUNT_HEIGHT#Count
-                type COUNT_VERT = self.TT#COUNT_VERT#Count
-            }
-        }*/
-        
         def setSHeight = new Base {
             type ST = BaseState {
                 type COUNT_LENGTH = self.ST#COUNT_LENGTH
@@ -133,14 +97,6 @@ object StateO {
             }
         }
 
-        //def setEdge = new SpecsWith {
-        /*def setEdge = new Base {
-            type TT = self.TT {
-                type COUNT_EDGE = self.TT#COUNT_EDGE#Count
-                type COUNT_VERT = self.TT#COUNT_VERT#Count
-            }
-        }*/
-        
         def setSEdge = new Base {
             type ST = BaseState {
                 type COUNT_LENGTH  = self.ST#COUNT_LENGTH
@@ -155,7 +111,9 @@ object StateO {
 
     }
 
-    //Starting point: nothing is set
+    /**
+     * Starting point: nothing is set
+     */
     def apply() = new Base {
       
         type ST = BaseState {
@@ -168,19 +126,12 @@ object StateO {
             type COUNT_VERT = ZERO
         }
           
-        /*type TT = {
-            type COUNT_LENGTH = ZERO
-            type COUNT_WIDTH = ZERO
-            type COUNT_AREA = ZERO
-            type COUNT_BASE = ZERO
-            type COUNT_HEIGHT = ZERO
-            type COUNT_EDGE = ZERO
-            type COUNT_VERT = ZERO
-        }*/
     }
         
-    //Required ending point: two base measures, one height measure,
-    //no single parameter more than once
+    /** 
+     *  Required ending point: two base measures, one height measure,
+     *  no single parameter more than once
+     */
     type SCompleteSpecs = Base {
           type ST <: BaseState {
               type COUNT_LENGTH <: ZERO_OR_ONE
@@ -193,21 +144,6 @@ object StateO {
           }
     }
 
-    /*
-    //Required ending point: two base measures, one height measure,
-    //no single parameter more than once
-    type CompleteSpecs = Base {
-        type TT <: {
-            type COUNT_LENGTH <: ZERO_OR_ONE
-            type COUNT_WIDTH <: ZERO_OR_ONE
-            type COUNT_AREA <: ZERO_OR_ONE
-            type COUNT_BASE = TWO
-            type COUNT_HEIGHT <: ZERO_OR_ONE
-            type COUNT_EDGE <: ZERO_OR_ONE
-            type COUNT_VERT = ONE
-        }
-    }*/
-    
     //Calc1 includes the first set of values that can be calculated
     class Calc2(spec:SCompleteSpecs) {
     }
@@ -220,15 +156,6 @@ object StateO {
         def build = new Calc2(spec)
     }
 
-    /*
-    //Calc1 includes the first set of values that can be calculated
-    class Calc1(spec:CompleteSpecs) {
-    }
-    
-    implicit def specsOK(spec:CompleteSpecs) = new  {
-        def build = new Calc1(spec)
-    }
-  */
 }
 
 object StatefullBuilderV2 {
