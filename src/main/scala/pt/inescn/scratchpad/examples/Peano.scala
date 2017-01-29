80896f7b2b31ff9dcb9783e39d9e4519d0c49486
Hugo Ferreira <hmf@inesctec.pt>
2017-01-29 13:00:52 +0000

    Peano factorial nearly complete.

--- src/main/scala/pt/inescn/scratchpad/examples/Peano.scala
+++ src/main/scala/pt/inescn/scratchpad/examples/Peano.scala
@@ -49,19 +49,32 @@ object Peano {
     implicit def add[A <: Nat, B <: Nat, C <: Nat](implicit ev : Sum[Succ[A],B,C]) =  new Sum[A, Succ[B], C] { }    
   }
 
-  //trait Prod[A <: Nat, B <: Nat, C <: Nat]
-  /*
+  trait Prod[A <: Nat, B <: Nat, C <: Nat]
+  
   object Prod {
     import Nat._0
     
-    implicit def prod[A <: Nat] =  new Prod[A, _0, _0] {}
+    implicit def prod0[A <: Nat] =  new Prod[A, _0, _0] {}
     // A * S(B) =  (A * B) + A 
     // A * B = D : recursion
     // D + A : do sum
     implicit def prod[A <: Nat, B <: Nat, C <: Nat, D <: Nat](implicit ev1 : Prod[A,B,D], ev2 : Sum[A,D,C]) =  
       new Prod[A, Succ[B], C] { }    
   }
-  */
+
+  trait Fact[A <: Nat, B <: Nat]
+  
+  object Fact {
+    import Nat.{_0, _1}
+    
+    implicit def fact0 =  new Fact[_0, _1] {}
+    // S(A)! = A! * S(A)  
+    // A!  = D : recursion
+    // D * S(A) : do product
+    //implicit def fact[A <: Nat, B <: Nat, C <: Nat, D <: Nat](implicit ev1 : Fact[A,D], ev2 : Prod[A,D,C]) =  
+    implicit def fact[A <: Nat, B <: Nat, C <: Nat, D <: Nat](implicit ev1 : Fact[A,D],  ev2 : Prod[Succ[A],D,C]) =  
+      new Fact[Succ[A], C] {  println("Fact[Succ[A], C]") }    
+  }
   
   def main( args: Array[ String ] ) {
     import Nat._
@@ -70,13 +83,21 @@ object Peano {
     println( pp0 )
 
     // TODO: extract solution
-    type R[X <: Nat] = Sum[_2,_3, X]
+    type S[X <: Nat] = Sum[_2,_3, X]
     // val x = toInt[X]
     implicitly[Sum[_2,_3,_5]]
     implicitly[Sum[_4,_5,_9]]
     // TODO: equals
     // implicitly[Sum[_5,_5, X] =:= Sum[_6,_4,X] ]
-    
+
+    implicitly[Prod[_2,_3,_6]]
+    implicitly[Prod[_2,_4,_8]]
+
+    val x = Fact.fact0
+    type F[X <: Nat] = Fact[_1,_1]
+    implicitly[Fact[_1,_1]]
+    implicitly[Fact[_2,_2]]
+    implicitly[Fact[_3,_6]] 
   }
 
 }
