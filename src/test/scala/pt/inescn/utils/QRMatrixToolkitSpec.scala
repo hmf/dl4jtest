3c4ca12f4d7cbbb1a0f7d5c8af42cbdb12beb04b
Hugo Ferreira <hmf@inesctec.pt>
2017-03-06 18:12:38 +0000

    Reorginazing scala-tests - using WordSpec.

--- src/test/scala/pt/inescn/utils/QRMatrixToolkitSpec.scala
+++ src/test/scala/pt/inescn/utils/QRMatrixToolkitSpec.scala
@@ -6,6 +6,7 @@ import collection.mutable.Stack
 import org.scalatest._
 
 //import pt.inescn.utils.TestUtils._
+// import scala.collection.JavaConversions._
 
 import no.uib.cipr.matrix.DenseMatrix
 import no.uib.cipr.matrix.DenseVector
@@ -28,25 +29,47 @@ import no.uib.cipr.matrix.Vector
  *
  *
  */
-class QRMatrixToolkitSpec extends FlatSpec with Matchers {
+class QRMatrixToolkitSpec extends WordSpec with Matchers {
 
-  "check rank" should "get thin matrix rank number for linear independent columns" in {
-    
-    val rand = Matrices.random( 6, 4 )
-    val A = new DenseMatrix( rand.numRows(), rand.numRows() )
-    rand.transBmult( rand, A )
-    val qrp = QRP.factorize( A )
+  "Matrices" when {
+    "genrated with random elements " should {
+      "being thin have only linear independent columns" in {
+        val rand = Matrices.random( 6, 4 )
+        val A = new DenseMatrix( rand.numRows(), rand.numRows() )
+        rand.transBmult( rand, A )
+        val qrp = QRP.factorize( A )
 
-    Math.min( rand.numRows(), rand.numColumns() ) shouldBe qrp.getRank()
+        Math.min( rand.numRows(), rand.numColumns() ) shouldBe qrp.getRank()
+      }
+      "being fat have only linear independent columns" in {
+        val rand = Matrices.random( 4, 6 )
+        val A = new DenseMatrix( rand.numRows(), rand.numRows() )
+        rand.transBmult( rand, A )
+        val qrp = QRP.factorize( A )
+
+        Math.min( rand.numRows(), rand.numColumns() ) shouldBe qrp.getRank()
+      }
+    }
   }
 
-  it should "get fat matrix rank number for linear independent columns" in {
-    val rand = Matrices.random(4, 6)
-    val A = new DenseMatrix(rand.numRows(), rand.numRows())
-    rand.transBmult(rand, A)
-    val qrp = QRP.factorize(A)
+  "The sub-matrices of the simple 1/0 matrix" when {
+    
+    val c1 = Array( 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 )
+    val c2 = Array( 1.0, 1.0, 1.0, 0.0, 0.0, 0.0 )
+    val c3 = Array(  0.0, 0.0, 0.0, 1.0, 1.0, 1.0 )
+    val c4 = Array(  1.0, 0.0, 0.0, 1.0, 0.0, 0.0 )
+    val c5 = Array(  0.0, 1.0, 0.0, 0.0, 1.0, 0.0 )
+    val c6 = Array(  0.0, 0.0, 1.0, 0.0, 0.0, 1.0 )
 
-    Math.min(rand.numRows(), rand.numColumns()) shouldBe qrp.getRank()
+    val threshold = 1e-7
+    
+    "checked for rank" should {
+        "give the same result as SVD 1" in {
+          
+        }
+        
+      }
+    
   }
 
 }
